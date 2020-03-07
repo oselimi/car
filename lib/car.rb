368ba5041fd20bb69834dedda72040bacd3da6c3
oselimi <selimiosman94@gmail.com>
2020-03-07 17:41:18 +0100

    upload My Car

--- lib/car.rb
+++ lib/car.rb
@@ -1,20 +1,48 @@
 require "car/version"
 
-module Car
-  class MyCar
-    attr_accessor :color, :current_speed
-    attr_reader :year
+  module Car
+    class MyCar
+      SPEED_OFFSET = 15
 
-    def initialize(year,color,model)
-      @year = year
-      @color = color
-      @model = model
-      @current_speed = 0
+      attr_accessor :color, :current_speed, :price
+      attr_reader :year, :defects, :my_price
+
+
+      def initialize(year,color,model)
+        @year = year
+        @color = color
+        @model = model
+        @current_speed = 0
+        @defects = []
+        @price = []
+        @my_price = 5000
+      end
+
+      def speed_up(increase = SPEED_OFFSET)
+        @current_speed += increase
+      end
+
+      def slow_down(slowly = SPEED_OFFSET)
+        @current_speed -= slowly
+      end
+
+      def spray_paint(col)
+        @color = col
+      end
+
+      def add_defects(defects)
+        @defects << defects
+      end
+
+      def service_info
+        df = @defects.join(', ')
+        "Defects to be fixed: #{df}"
+      end
+
+      def for_sale(price)
+        @price << price
+        @price.join(', ')
+        "Thank you for your price: #{price} but my price is #{my_price}"
+      end
     end
-
-    def speed_up
-        @current_speed += 100
-    end
-
   end
-end
--- spec/mycar_spec.rb
+++ spec/mycar_spec.rb
@@ -1,16 +1,54 @@
 RSpec.describe Car::MyCar do
-    it "Creates instance of my car" do
-        volvo = Car::MyCar.new(2020, "blue", "volvo")
+    let(:volvo) { Car::MyCar.new(2020, "blue", "volvo") }
+    let(:failure) { "water pomp , chnage oil, breaking problem" }
+    it "creates instance of my car" do
         expect(volvo).to be_instance_of(Car::MyCar)
     end
 
-    it "Has starting speed defind to 0" do
-        volvo = Car::MyCar.new(2020, "blue", "volvo")
-        expect(volvo.current_speed).to eq(0) 
+    it "has starting speed defind to 0" do
+        expect(volvo.current_speed).to eq(0)
     end
 
-    it "Limited speed in my country is 130kmh" do
-        volvo = Car::MyCar.new(2020, "blue", "volvo")
-        expect(volvo.speed_up).to be <= 130
+    it "limited speed in my country is 130kmh" do
+        expect(volvo.current_speed).to be <= 130
+    end
+
+    it "speed up per 10 "do
+        volvo.speed_up
+        expect(volvo.current_speed).to eq(Car::MyCar::SPEED_OFFSET)
+    end
+
+    it "slowly driving with argument" do
+      volvo.slow_down(30)
+      expect(volvo.current_speed).to eq(-30)
+    end
+
+    it "slowly driving" do
+      volvo.slow_down
+      expect(volvo.current_speed).to eq(-Car::MyCar::SPEED_OFFSET)
+    end
+
+    it "changes the color of the car" do
+      volvo.spray_paint :black
+      expect(volvo.color).to eq(:black)
+    end
+
+    it "has empty" do
+      expect(volvo.defects).to be_empty
+    end
+
+    it "method for adds defects" do
+      volvo.add_defects(failure)
+      expect(volvo.defects).to include(failure)
+    end
+
+    it "method for service raport" do
+      volvo.add_defects(failure)
+      expect(volvo.service_info).to eq("Defects to be fixed: #{failure}")
+    end
+
+    it "has many defects and i like to sell"do
+      volvo.for_sale(2000)
+      expect(volvo.price).to include(2000)
     end
 end
